d040e4807dc74f0a7a07f2a4692b488d1a387fe1
Kurt Mohler <kmohler@quansight.com>
2020-04-21 22:46:59 -0500

    Add sum performance script

---
+++ sum/sum_perf.py
@@ -0,0 +1,35 @@
+import torch
+import time
+
+torch.set_num_threads(1)
+
+timed_iters = 100
+
+for device in ['cpu']:
+    for dtype in [torch.float, torch.int, torch.bool, torch.uint8]:
+        for tensor_size in [1_000, 10_000, 100_000, 1_000_000, 10_000_000]:
+            if dtype == torch.float:
+                a = torch.rand(tensor_size, dtype=dtype, device=device)
+            else:
+                a = torch.randint(2, [tensor_size], dtype=dtype, device=device)
+                a = a == 1
+
+                if dtype == torch.uint8:
+                    a = a.to(dtype)
+
+            for warmup_iter in range(2):
+                start_time = time.time()
+                for i in range(timed_iters):
+                    a_sum = a.sum()
+                # if device == 'cuda':
+                #     torch.cuda.synchronize()
+                total_time = time.time() - start_time
+
+            time_per_iter = total_time / timed_iters
+
+            print("%s %s %s %f" % (
+                device,
+                dtype,
+                tensor_size,
+                time_per_iter
+            ))
