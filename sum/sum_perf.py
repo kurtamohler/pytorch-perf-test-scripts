ff9da287a9e9d62812d8375ec447ede591a29bcf
Kurt Mohler <kmohler@quansight.com>
2020-04-23 12:24:32 -0500

    Fix int tensor creation

--- sum/sum_perf.py
+++ sum/sum_perf.py
@@ -7,20 +7,18 @@ timed_iters = 100
 
 for device in ['cpu']:
     for dtype in [torch.float, torch.int, torch.bool, torch.uint8]:
+        out_dtype = torch.int if dtype != torch.float else torch.float
         for tensor_size in [1_000, 10_000, 100_000, 1_000_000, 10_000_000]:
             if dtype == torch.float:
                 a = torch.rand(tensor_size, dtype=dtype, device=device)
+            elif dtype == torch.int:
+                a = torch.randint(100, [tensor_size], dtype=dtype, device=device)
             else:
                 a = torch.randint(2, [tensor_size], dtype=dtype, device=device)
-                a = a == 1
-
-                if dtype == torch.uint8:
-                    a = a.to(dtype)
-
             for warmup_iter in range(2):
                 start_time = time.time()
                 for i in range(timed_iters):
-                    a_sum = a.sum()
+                    a_sum = a.sum(dtype=out_dtype)
                 # if device == 'cuda':
                 #     torch.cuda.synchronize()
                 total_time = time.time() - start_time
