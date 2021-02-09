8ca11fc75ed1d6d9ba664fba05d1aa917cadf400
Kurt Mohler <kmohler@quansight.com>
2021-02-08 18:27:30 -0600

    Update norm perf script

--- linalg_norm/norm_perf.py
+++ linalg_norm/norm_perf.py
@@ -1,77 +1,110 @@
 import torch
 import time
 
+torch.manual_seed(0)
+
 def measure_time(input, f, args, kwargs, num_iters):
+    need_sync = input.device.type == 'cuda'
+
     for warmup in range(2):
         start = time.time()
 
         for iter in range(num_iters):
             result = f(input, *args, **kwargs)
 
+        if need_sync:
+            torch.cuda.synchronize(input.device)
         end = time.time() 
 
     return (end - start) / num_iters, result
 
 
+def custom_norm(input, order):
+    if order == 1:
+        return input.abs().pow(order).sum().pow(1. / order)
+    elif order == 2:
+        input_abs = input.abs()
+        return (input_abs * input_abs).sum().sqrt()
+    elif order == float('inf'):
+        return input.abs().amax()
+    elif order == -float('inf'):
+        return input.abs().amin()
+    else:
+        return input.abs().pow(order).sum().pow(1. / order)
+
+
 methods = {
     'norm': torch.norm,
     'linalg_norm': torch.linalg.norm,
-    'custom-order-1': lambda input, order: input.abs().pow(order).sum().pow(1. / order),
-    'custom': lambda input, order: input.abs().pow(order).sum().pow(1. / order)
+    'linalg_vector_norm': torch.linalg.vector_norm,
+    'custom': custom_norm,
 }
 
 
-num_iters = 10000
+num_inputs = 10
 
 print('NOTE: N/A means that the operation is not supported')
 print()
-print('dtype device order input-size method time-seconds')
+print(f'dtype device order input-size {" ".join(methods.keys())}')
+print()
 for dtype in [torch.float, torch.cfloat, torch.double, torch.cdouble]:
     for device in ['cpu', 'cuda']:
-        for order in [1, 2, 3]:
+        for order in [1, 2, 3, float('inf'), -float('inf')]:
             for input_size in [3, 8_000, 16_000, 44_100, 32 * 44_100]:
-                input = torch.randn(input_size, dtype=dtype, device=device)
-                results = []
-                for method_name, f in methods.items():
-                    if method_name == 'custom' and order == 1:
-                        continue
-                    elif method_name == 'custom-order-1' and order != 1:
-                        continue
-
-                    if method_name == 'norm':
-                        kwargs = {
-                            'p': order
-                        }
-                        args = []
-                    elif method_name == 'linalg_norm':
-                        kwargs = {
-                            'ord': order
-                        }
-                        args = []
+                # CPU is much slower than CUDA, so it needs fewer timed iterations
+                if device == 'cpu':
+                    # When tensor can no longer fit into lower cache levels,
+                    # norm performance significantly drops off, so we can
+                    # decrease timed iterations linearly from that point
+                    if input_size >= 44_100:
+                        num_iters = int(100 / (input_size / 44_100))
+                        if num_iters < 10:
+                            num_iters = 10
                     else:
-                        kwargs = {}
-                        args = [order]
+                        num_iters = 200
+                else:
+                    num_iters = 500
+                times_for_all_inputs = []
+                for input_num in range(num_inputs):
+                    input = torch.randn(input_size, dtype=dtype, device=device)
+                    results = []
+                    times_for_input = []
+                    for method_name, f in methods.items():
 
-                    try:
-                        time_seconds, result = measure_time(input, f, args, kwargs, 100)
-                        results.append(result)
-                    except RuntimeError:
-                        #print(f'{device} {input_size} {order} {method_name} N/A')
-                        #continue
-                        time_seconds = 'N/A'
+                        if method_name == 'norm':
+                            kwargs = {
+                                'p': order
+                            }
+                            args = []
+                        elif method_name == 'linalg_norm':
+                            kwargs = {
+                                'ord': order
+                            }
+                            args = []
+                        elif method_name == 'linalg_vector_norm':
+                            kwargs = {
+                                'ord': order
+                            }
+                            args = []
+                        else:
+                            kwargs = {}
+                            args = [order]
 
-                    print(f'{dtype} {device} {order} {input_size} {method_name} {time_seconds}')
+                        try:
+                            time_seconds, result = measure_time(input, f, args, kwargs, num_iters)
+                            results.append(result)
+                        except RuntimeError:
+                            time_seconds = 'N/A'
+
+                        times_for_input.append(time_seconds)
+                    times_for_all_inputs.append(times_for_input)
+                avg_times = torch.tensor(times_for_all_inputs, dtype=torch.float64).mean(dim=0).tolist()
                 # Make sure that the difference between results of the different
                 # methods is very small, so we know they are all calculating the
                 # same thing
                 for result_ind in range(1, len(results)):
                     if results[0] != results[result_ind]:
                         assert ((results[0] - results[result_ind]) / results[0]).abs() < 0.001
+                print(f'{dtype} {device} {order} {input_size} {" ".join([str(t) for t in avg_times])}')
             print()
-
-
-
-
-
-
 
--- linalg_norm/run_single_core_norm_perf.sh
+++ linalg_norm/run_single_core_norm_perf.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-OMP_NUM_THREADS=1 numactl --membind 0 --cpubind 0 python $(dirname "$0")/norm_perf.py
+#OMP_NUM_THREADS=1 numactl --membind 0 --cpubind 0 python $(dirname "$0")/norm_perf.py
+
+#ATEN_CPU_CAPABILITY=default OMP_NUM_THREADS=1 taskset 0x10 python $(dirname "$0")/norm_perf.py
+
+OMP_NUM_THREADS=1 taskset 0x10 python $(dirname "$0")/norm_perf.py
