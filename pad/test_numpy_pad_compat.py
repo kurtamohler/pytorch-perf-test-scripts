87b432cd97b6be13f12cf49f30621c11794a3379
Kurt Mohler <kurtamohler@gmail.com>
2021-07-01 16:36:51 -0500

    Clarify some comments

--- pad/test_numpy_pad_compat.py
+++ pad/test_numpy_pad_compat.py
@@ -1,12 +1,14 @@
 # This script excercises many different calls to torch.nn.functional.pad
 # and checks that the result matches the equivalent numpy.pad call.
+
 import torch
 import numpy as np
 import itertools
 from torch.testing._internal.common_utils import make_tensor
 
+# List of all the test cases to run
 test_cases = [
-    # Input size, Numpy pad mode, pad width (numpy format), kwargs numpy, kwargs torch
+    # Input size, numpy pad mode, numpy pad width, numpy kwargs, torch kwargs
     ((10, 10), 'constant', ((2, 2), (2, 2)), {}, {}),
     ((10, 10), 'constant', ((2, 2), (2, 2)), {'constant_values': 10}, {'value': 10}),
     ((5, 2, 4, 1, 7), 'constant', ((1, 2), (6, 4), (10, 7), (3, 3), (0, 0)), {'constant_values': 89}, {'value': 89}),
@@ -48,7 +50,8 @@ devices = [
     'cuda'
 ]
 
-# Get the equivalent
+
+# Get the PyTorch equivalent of a Numpy pad mode
 def get_mode_torch(mode_numpy):
     if mode_numpy in ['reflect', 'constant']:
         return mode_numpy
@@ -59,13 +62,18 @@ def get_mode_torch(mode_numpy):
     else:
         assert False, f'NumPy mode "{mode_numpy}" has no PyTorch equivalent'
 
-# Numpy's pad width arg must be a 2-D sequence of paired before- and
-# after-padding for each dimension. Numpy supports other formats, but those are
-# not compatible with PyTorch. PyTorch's pad width arg has its pairs in reverse
-# order, and the arg is just a 1-D sequence, where each contiguous pair of
-# elements makes up the before- and after-padding for one dimension. Also,
-# for all modes except 'constant', we cannot include the first two dimensions
-# in the PyTorch pad_width
+
+# Get the PyTorch equivalent of a Numpy pad width arg.
+#
+# Details:
+#
+#   Numpy's pad width arg must be a 2-D sequence of paired before- and
+#   after-padding for each dimension. Numpy supports other formats, but those
+#   are not compatible with PyTorch. PyTorch's pad width arg has its pairs in
+#   reverse order, and the arg is just a 1-D sequence, where each contiguous
+#   pair of elements makes up the before- and after-padding for one dimension.
+#   Also, for all modes except 'constant', we cannot include the first two
+#   dimensions in the PyTorch pad width arg.
 def get_pad_width_torch(pad_width_numpy, mode_torch):
     pad_width_torch = tuple(itertools.chain.from_iterable(reversed(pad_width_numpy)))
 
@@ -98,7 +106,10 @@ for device, dtype in itertools.product(devices, dtypes):
                 # If this case is not implemented in torch, we don't need to
                 # worry about compatibility
                 print(f'SKIP: {e_str} in {case_name}')
-                pass
+
+                # However, we should never get this exception with float and
+                # double
+                assert dtype not in [torch.float, torch.double]
 
             else:
                 raise e
