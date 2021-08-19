8d49a836a8c4622bec1835b2a480cccd6b9a5e84
Kurt Mohler <kmohler@quansight.com>
2021-08-19 15:04:32 -0500

    Add comparison with torch.nn.functional.pad

--- pad/perf.py
+++ pad/perf.py
@@ -3,6 +3,8 @@ from torch.utils import benchmark
 import numpy as np
 from itertools import product
 
+torch.set_num_threads(1)
+
 devices = [
     'cpu',
     'cuda'
@@ -13,44 +15,100 @@ dtypes = [
     torch.double
 ]
 
-cases = [
-    # input size, pad_width, constant_values
-    ((10,), 10, None),
-    ((100,), 100, None),
-    ((1000,), 1000, None),
-    ((10000,), 10000, None),
-    ((10000,), 10, None),
-    ((10, 10, 10), 10, None),
-    ((10, 10, 10), ((1000,), (0,), (0,)), None),
-    ((20, 10, 10), 10, None),
-    ((30, 10, 10), 10, None),
-    ((100, 10, 10), 10, None),
-    ((10, 10, 10), 10, ((0, 1), (2, 3), (4, 5))),
-    ((10, 10, 10), ((10, 10), (10, 10), (10, 10)), ((0, 1), (2, 3), (4, 5))),
-    ((100, 100, 100), ((10, 10), (10, 10), (10, 10)), None),
-]
+####################################
+# Compare torch.pad with numpy.pad #
+####################################
 
-num_iters = 100
+def bench1():
+    cases = [
+        # input size, pad_width, constant_values
+        ((10,), 10, None),
+        ((100,), 100, None),
+        ((1000,), 1000, None),
+        ((10000,), 10000, None),
+        ((10000,), 10, None),
+        ((10, 10, 10), 10, None),
+        ((10, 10, 10), ((1000,), (0,), (0,)), None),
+        ((20, 10, 10), 10, None),
+        ((30, 10, 10), 10, None),
+        ((100, 10, 10), 10, None),
+        ((10, 10, 10), 10, ((0, 1), (2, 3), (4, 5))),
+        ((10, 10, 10), ((10, 10), (10, 10), (10, 10)), ((0, 1), (2, 3), (4, 5))),
+        ((100, 100, 100), ((10, 10), (10, 10), (10, 10)), None),
+    ]
 
-print('device dtype case_idx time_torch time_numpy torch_speedup')
-print()
+    num_iters = 10000
 
-for device, dtype, (case_idx, (input_size, pad_width, constant_values)) in product(devices, dtypes, enumerate(cases)):
-    time_numpy = benchmark.Timer(
-        setup=f'import numpy as np; a = torch.randn({input_size}, dtype={dtype}).numpy()',
-        stmt=f'np.pad(a, {pad_width}, constant_values={constant_values})'
-    ).timeit(num_iters).mean
+    print('====================================')
+    print('compare with torch.pad')
+    print()
+    print('device dtype case_idx time_torch time_numpy torch_speedup')
+    print()
 
-    time_torch = benchmark.Timer(
-        setup=f'a = torch.randn({input_size}, dtype={dtype}, device="{device}")',
-        stmt=f'torch.pad(a, {pad_width}, constant_values={constant_values})'
-    ).timeit(num_iters).mean
+    for device, dtype, (case_idx, (input_size, pad_width, constant_values)) in product(devices, dtypes, enumerate(cases)):
+        time_numpy = benchmark.Timer(
+            setup=f'import numpy as np; a = torch.randn({input_size}, dtype={dtype}).numpy()',
+            stmt=f'np.pad(a, {pad_width}, constant_values={constant_values})'
+        ).timeit(num_iters).mean
 
-    torch_speedup = time_numpy / time_torch
+        time_torch = benchmark.Timer(
+            setup=f'a = torch.randn({input_size}, dtype={dtype}, device="{device}")',
+            stmt=f'torch.pad(a, {pad_width}, constant_values={constant_values})'
+        ).timeit(num_iters).mean
+
+        torch_speedup = time_numpy / time_torch
 
 
-    print(f'{device} {dtype} {case_idx} {time_torch:.2e} {time_numpy:.2e} {torch_speedup:.2f}')
-    if case_idx == (len(cases) - 1):
-        print()
+        print(f'{device} {dtype} {case_idx} {time_torch:.2e} {time_numpy:.2e} {torch_speedup:.2f}')
+        if case_idx == (len(cases) - 1):
+            print()
 
 
+##################################################
+# Compare torch.pad with torch.nn.functional.pad #
+##################################################
+
+def bench2():
+    cases = [
+        # input size, pad_width_new, pad_width_old, constant_values_new, constant_values_old
+        ((10,), 10, (10, 10), None, None),
+        ((100,), 100, (100, 100), None, None),
+        ((1000,), 1000, (1000, 1000), None, None),
+        ((10000,), 10000, (10000, 10000), None, None),
+        ((10000,), 10, (10, 10), None, None),
+        ((10, 10, 10), 10, (10, 10, 10, 10, 10, 10), None, None),
+        ((10, 10, 10), ((1000,), (0,), (0,)), (1000, 1000, 0, 0, 0, 0), None, None),
+        ((20, 10, 10), 10, (10, 10), None, None),
+        ((30, 10, 10), 10, (10, 10), None, None),
+        ((100, 10, 10), 10, (10, 10), None, None),
+    ]
+
+    num_iters = 10000
+
+
+    print('====================================')
+    print('compare with torch.nn.functional.pad')
+    print()
+    print('device dtype case_idx time_new time_old torch_speedup')
+    print()
+
+    for device, dtype, (case_idx, (input_size, pad_width_new, pad_width_old, constant_values_new, constant_values_old)) in product(devices, dtypes, enumerate(cases)):
+        time_old = benchmark.Timer(
+            setup=f'a = torch.randn({input_size}, dtype={dtype}, device="{device}")',
+            stmt=f'torch.nn.functional.pad(a, {pad_width_old}, value={0 if constant_values_old is None else constant_values_old})'
+        ).timeit(num_iters).mean
+
+        time_new = benchmark.Timer(
+            setup=f'a = torch.randn({input_size}, dtype={dtype}, device="{device}")',
+            stmt=f'torch.pad(a, {pad_width_new}, constant_values={constant_values_new})'
+        ).timeit(num_iters).mean
+
+        new_speedup = time_old / time_new
+
+
+        print(f'{device} {dtype} {case_idx} {time_new:.2e} {time_old:.2e} {new_speedup:.2f}')
+        if case_idx == (len(cases) - 1):
+            print()
+
+bench1()
+bench2()
